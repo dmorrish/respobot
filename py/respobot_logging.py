c469613c3f5f220f2bd789975f68839987be5a5f
Deryk Morrish <dmorrish@gmail.com>
2023-07-24 09:39:22 -0400

    Tweaked log formatting to account for double quotes in discord logs.

--- py/respobot_logging.py
+++ py/respobot_logging.py
@@ -10,26 +10,26 @@ logger_discord = logging.getLogger('discord')
 logger_discord.propagate = False
 logger_discord.setLevel(logging.INFO)
 handler = logging.FileHandler(filename=env.BOT_DIRECTORY + env.LOG_SUBDIRECTORY + 'discord.log', encoding='utf-8', mode='a')
-handler.setFormatter(logging.Formatter('{"args": "%(args)s", "created": %(created)f, "exc_text": "", "filename": "%(filename)s", "funcName": "%(funcName)s", "levelname": "%(levelname)s", "levelno": %(levelno)d, "lineno": %(lineno)d, "module": "%(module)s", "msecs": %(msecs)d, "msg": "%(message)s", "name": "%(name)s", "pathname": "", "process": %(process)d, "processName": "%(processName)s", "relativeCreated": %(relativeCreated)d, "stack_info": "%(stack_info)s", "thread": %(thread)d, "threadName": "%(threadName)s", "extra_column": ""},'))
+handler.setFormatter(logging.Formatter("{'args': null, 'created': %(created)f, 'exc_text': '', 'filename': '%(filename)s', 'funcName': '%(funcName)s', 'levelname': '%(levelname)s', 'levelno': %(levelno)d, 'lineno': %(lineno)d, 'module': '%(module)s', 'msecs': %(msecs)d, 'msg': '%(message)s', 'name': '%(name)s', 'pathname': '', 'process': %(process)d, 'processName': '%(processName)s', 'relativeCreated': %(relativeCreated)d, 'stack_info': '%(stack_info)s', 'thread': %(thread)d, 'threadName': '%(threadName)s'},"))
 logger_discord.addHandler(handler)
 
 logger_pyracing = logging.getLogger('pyracing')
 logger_pyracing.propagate = False
 logger_pyracing.setLevel(logging.INFO)
 handler2 = logging.FileHandler(filename=env.BOT_DIRECTORY + env.LOG_SUBDIRECTORY + 'pyracing.log', encoding='utf-8', mode='a')
-handler2.setFormatter(logging.Formatter('{"args": "%(args)s", "created": %(created)f, "exc_text": "", "filename": "%(filename)s", "funcName": "%(funcName)s", "levelname": "%(levelname)s", "levelno": %(levelno)d, "lineno": %(lineno)d, "module": "%(module)s", "msecs": %(msecs)d, "msg": "%(message)s", "name": "%(name)s", "pathname": "", "process": %(process)d, "processName": "%(processName)s", "relativeCreated": %(relativeCreated)d, "stack_info": "%(stack_info)s", "thread": %(thread)d, "threadName": "%(threadName)s", "extra_column": ""},'))
+handler2.setFormatter(logging.Formatter("{'args': null, 'created': %(created)f, 'exc_text': '', 'filename': '%(filename)s', 'funcName': '%(funcName)s', 'levelname': '%(levelname)s', 'levelno': %(levelno)d, 'lineno': %(lineno)d, 'module': '%(module)s', 'msecs': %(msecs)d, 'msg': '%(message)s', 'name': '%(name)s', 'pathname': '', 'process': %(process)d, 'processName': '%(processName)s', 'relativeCreated': %(relativeCreated)d, 'stack_info': '%(stack_info)s', 'thread': %(thread)d, 'threadName': '%(threadName)s'},"))
 logger_pyracing.addHandler(handler2)
 
 logger_respobot = logging.getLogger('respobot')
 logger_respobot.propagate = False
 logger_respobot.setLevel(logging.INFO)
 handler3 = logging.FileHandler(filename=env.BOT_DIRECTORY + env.LOG_SUBDIRECTORY + 'respobot.log', encoding='utf-8', mode='a')
-handler3.setFormatter(logging.Formatter('{"args": "%(args)s", "created": %(created)f, "exc_text": "", "filename": "%(filename)s", "funcName": "%(funcName)s", "levelname": "%(levelname)s", "levelno": %(levelno)d, "lineno": %(lineno)d, "module": "%(module)s", "msecs": %(msecs)d, "msg": "%(message)s", "name": "%(name)s", "pathname": "", "process": %(process)d, "processName": "%(processName)s", "relativeCreated": %(relativeCreated)d, "stack_info": "%(stack_info)s", "thread": %(thread)d, "threadName": "%(threadName)s", "extra_column": ""},'))
+handler3.setFormatter(logging.Formatter("{'args': null, 'created': %(created)f, 'exc_text': '', 'filename': '%(filename)s', 'funcName': '%(funcName)s', 'levelname': '%(levelname)s', 'levelno': %(levelno)d, 'lineno': %(lineno)d, 'module': '%(module)s', 'msecs': %(msecs)d, 'msg': '%(message)s', 'name': '%(name)s', 'pathname': '', 'process': %(process)d, 'processName': '%(processName)s', 'relativeCreated': %(relativeCreated)d, 'stack_info': '%(stack_info)s', 'thread': %(thread)d, 'threadName': '%(threadName)s'},"))
 logger_respobot.addHandler(handler3)
 
 logger_test = logging.getLogger('logtest')
 logger_test.propagate = False
 logger_test.setLevel(logging.INFO)
 handler4 = logging.FileHandler(filename=env.BOT_DIRECTORY + env.LOG_SUBDIRECTORY + 'logtest.log', encoding='utf-8', mode='a')
-handler4.setFormatter(logging.Formatter('{"args": "%(args)s", "created": %(created)f, "exc_text": "", "filename": "%(filename)s", "funcName": "%(funcName)s", "levelname": "%(levelname)s", "levelno": %(levelno)d, "lineno": %(lineno)d, "module": "%(module)s", "msecs": %(msecs)d, "msg": "%(message)s", "name": "%(name)s", "pathname": "", "process": %(process)d, "processName": "%(processName)s", "relativeCreated": %(relativeCreated)d, "stack_info": "%(stack_info)s", "thread": %(thread)d, "threadName": "%(threadName)s", "extra_column": ""},'))
+handler4.setFormatter(logging.Formatter("{'args': null, 'created': %(created)f, 'exc_text': '', 'filename': '%(filename)s', 'funcName': '%(funcName)s', 'levelname': '%(levelname)s', 'levelno': %(levelno)d, 'lineno': %(lineno)d, 'module': '%(module)s', 'msecs': %(msecs)d, 'msg': '%(message)s', 'name': '%(name)s', 'pathname': '', 'process': %(process)d, 'processName': '%(processName)s', 'relativeCreated': %(relativeCreated)d, 'stack_info': '%(stack_info)s', 'thread': %(thread)d, 'threadName': '%(threadName)s'},"))
 logger_test.addHandler(handler4)
