3cc7471da7d088f26e82074d3666a4e7f6951434
Deryk Morrish <dmorrish@gmail.com>
2023-07-24 10:37:27 -0400

    Putting things back now that logging is working.

--- py/respobot.py
+++ py/respobot.py
@@ -12,7 +12,6 @@ from pyracing.client import Client as IracingClient
 from datetime import datetime, timezone
 import httpx
 import traceback
-import logging
 
 # RespoBot modules
 import constants
@@ -82,16 +81,10 @@ bot.add_cog(OnReactionAddCog(bot, db, ir, bot_state))
 
 @bot.event
 async def on_ready():
-    loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
-    print(loggers)
-
     await db.init()
     await bot.change_presence(activity=discord.Game(name="50 Cent: Bulletproof"))
 
     print("I'm alive!")
-
-    log.logger_test.info("Oh, hi there!")
-    log.logger_test.warning("Uh oh!")
 
     season_dates = await db.get_season_dates()
     if season_dates is None:
--- py/respobot_logging.py
+++ py/respobot_logging.py
@@ -26,10 +26,3 @@ logger_respobot.setLevel(logging.INFO)
 handler3 = logging.FileHandler(filename=env.BOT_DIRECTORY + env.LOG_SUBDIRECTORY + 'respobot.log', encoding='utf-8', mode='a')
 handler3.setFormatter(logging.Formatter("{'args': null, 'created': %(created)f, 'exc_text': '', 'filename': '%(filename)s', 'funcName': '%(funcName)s', 'levelname': '%(levelname)s', 'levelno': %(levelno)d, 'lineno': %(lineno)d, 'module': '%(module)s', 'msecs': %(msecs)d, 'msg': '%(message)s', 'name': '%(name)s', 'pathname': '', 'process': %(process)d, 'processName': '%(processName)s', 'relativeCreated': %(relativeCreated)d, 'stack_info': '%(stack_info)s', 'thread': %(thread)d, 'threadName': '%(threadName)s'},"))
 logger_respobot.addHandler(handler3)
-
-logger_test = logging.getLogger('logtest')
-logger_test.propagate = False
-logger_test.setLevel(logging.INFO)
-handler4 = logging.FileHandler(filename=env.BOT_DIRECTORY + env.LOG_SUBDIRECTORY + 'logtest.log', encoding='utf-8', mode='a')
-handler4.setFormatter(logging.Formatter("{'args': null, 'created': %(created)f, 'exc_text': '', 'filename': '%(filename)s', 'funcName': '%(funcName)s', 'levelname': '%(levelname)s', 'levelno': %(levelno)d, 'lineno': %(lineno)d, 'module': '%(module)s', 'msecs': %(msecs)d, 'msg': '%(message)s', 'name': '%(name)s', 'pathname': '', 'process': %(process)d, 'processName': '%(processName)s', 'relativeCreated': %(relativeCreated)d, 'stack_info': '%(stack_info)s', 'thread': %(thread)d, 'threadName': '%(threadName)s'},"))
-logger_test.addHandler(handler4)
