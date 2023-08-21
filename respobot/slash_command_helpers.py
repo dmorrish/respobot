6e078c807bfbd3813514ef60417103bfec4d0861
Deryk Morrish <dmorrish@gmail.com>
2023-08-21 19:30:27 -0400

    Changed memebr names to full name instead of only first. Added 'all_series' key to the iracing_series_dict for commands that work with a specific series and not a series that ran in a specific season.

--- respobot/slash_command_helpers.py
+++ respobot/slash_command_helpers.py
@@ -14,6 +14,7 @@ class SeasonStringError(Exception):
 class SlashCommandHelpers:
     db = None
     member_name_list = []
+    admin_member_name_list = []
     iracing_season_strings = []
     iracing_series_dict = {}
     current_season_string = ""
@@ -36,16 +37,34 @@ class SlashCommandHelpers:
     @classmethod
     async def refresh_members(self):
         new_member_name_list = []
+        new_admin_member_name_list = []
 
         member_dicts = await self.db.fetch_member_dicts()
 
         if member_dicts is not None and len(member_dicts) > 0:
             for member_dict in member_dicts:
-                new_member_name_list.append(member_dict['name'].split()[0])
+                new_member_name_list.append(member_dict['name'])
+                new_admin_member_name_list.append(f"{member_dict['uid']} : {member_dict['name']}")
 
-            new_member_name_list.sort()
+            split_list = []
+            for name in new_member_name_list:
+                split_list.append(name.split())
+
+            admin_split_list = []
+            for name in new_admin_member_name_list:
+                admin_split_list.append(name.split())
+
+            new_member_name_list = []
+            new_admin_member_name_list = []
+
+            for name in sorted(split_list, key=lambda x: x[-1]):
+                new_member_name_list.append(' '.join(name))
+
+            for name in sorted(admin_split_list, key=lambda x: x[-1]):
+                new_admin_member_name_list.append(' '.join(name))
 
         self.member_name_list = new_member_name_list
+        self.admin_member_name_list = new_admin_member_name_list
 
     @classmethod
     async def refresh_series(self):
@@ -64,6 +83,16 @@ class SlashCommandHelpers:
                     year -= 1
 
         new_iracing_series_dict = {}
+        new_iracing_series_dict['all_series'] = []
+
+        series_tuples = await self.db.get_all_series()
+
+        for series_tuple in series_tuples:
+            new_series_dict = {
+                "series_id": series_tuple["series_id"],
+                "series_name": series_tuple["series_name"]
+            }
+            new_iracing_series_dict['all_series'].append(new_series_dict)
 
         if new_iracing_season_strings is not None and len(new_iracing_season_strings) > 0:
             for season in new_iracing_season_strings:
@@ -78,19 +107,17 @@ class SlashCommandHelpers:
 
                 if series_info_tuples is not None and len(series_info_tuples) > 0:
                     for series_info_tuple in series_info_tuples:
-                        series_name = series_info_tuple[2]
-                        new_iracing_series_dict[season]['season_name_strings'].append(series_name)
-                        new_iracing_series_dict[season]['season_class_dict'][series_name] = []
-                        # series_id = await self.db.get_series_id_from_season_name(series_name, season_year=season_year, season_quarter=season_quarter)
-                        # season_id = await self.db.get_season_id(series_id, season_year, season_quarter)
                         season_id = series_info_tuple[0]
+                        season_name = series_info_tuple[2]
+                        new_iracing_series_dict[season]['season_name_strings'].append(season_name)
+                        new_iracing_series_dict[season]['season_class_dict'][season_name] = []
 
                         for car_class_tuple in car_class_tuples:
                             car_class_season_id = car_class_tuple[0]
                             car_class_short_name = car_class_tuple[2]
 
                             if car_class_season_id == season_id:
-                                new_iracing_series_dict[season]['season_class_dict'][series_name].append(car_class_short_name)
+                                new_iracing_series_dict[season]['season_class_dict'][season_name].append(car_class_short_name)
 
         self.current_season_string = new_current_season_string
         self.iracing_season_strings = new_iracing_season_strings
@@ -126,12 +153,20 @@ class SlashCommandHelpers:
         return (selected_year, selected_quarter)
 
     @classmethod
+    def get_current_season(self):
+        return self.parse_season_string(self.current_season_string)
+
+    @classmethod
     def get_member_list(self, ctx: discord.AutocompleteContext):
-        return [member_string for member_string in self.member_name_list if member_string.lower().startswith(ctx.value.lower())]
+        return [member_string for member_string in self.member_name_list if member_string.lower().find(ctx.value.lower()) > -1 or ctx.value == '' or ctx.value is None]
+
+    @classmethod
+    def get_admin_member_list(self, ctx: discord.AutocompleteContext):
+        return [member_string for member_string in self.admin_member_name_list if member_string.lower().find(ctx.value.lower()) > -1 or ctx.value == '' or ctx.value is None]
 
     @classmethod
     async def get_iracing_seasons(self, ctx: discord.AutocompleteContext):
-        return [season for season in self.iracing_season_strings if season.startswith(ctx.value.lower())]
+        return [season for season in self.iracing_season_strings if season.lower().find(ctx.value.lower()) > -1 or ctx.value == '' or ctx.value is None]
 
     @classmethod
     def get_series_list(self, ctx: discord.AutocompleteContext):
@@ -156,22 +191,32 @@ class SlashCommandHelpers:
         return [series for series in series_keyword_list if (series.lower().find(ctx.value.lower()) > -1 or ctx.value == '' or ctx.value is None)]
 
     @classmethod
+    def get_all_series_list(self, ctx: discord.AutocompleteContext):
+
+        series_keyword_list = [f"{series['series_name']} ({series['series_id']})" for series in self.iracing_series_dict['all_series']]
+
+        return [series_keyword for series_keyword in series_keyword_list if (series_keyword.lower().find(ctx.value.lower()) > -1 or ctx.value == '' or ctx.value is None)]
+
+    @classmethod
     async def get_series_classes(self, ctx: discord.AutocompleteContext):
         if 'season' in ctx.options:
             season_selected = ctx.options['season']
         else:
-            season_selected = None
+            season_selected = self.current_season_string
 
         if 'series' in ctx.options:
             series_selected = ctx.options['series']
         else:
             series_selected = None
 
-        if season_selected is None or season_selected == '' or series_selected is None or series_selected == '':
-            return ["Please slect a season and series first."]
+        if season_selected is None or season_selected == '':
+            season_selected = self.current_season_string
+        else:
+            if season_selected not in self.iracing_series_dict:
+                return ["Selected season not found in the series list. Let Deryk know because it means he sucks."]
 
-        if season_selected not in self.iracing_series_dict:
-            return ["Selected season not found in the series list. Let Deryk know because it means he sucks."]
+        if series_selected is None or series_selected == '':
+            return ["Please slect a series first."]
 
         if series_selected == constants.RESPO_SERIES_NAME:
             return ["Literally any fucking car at all."]
