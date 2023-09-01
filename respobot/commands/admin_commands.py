9060cba613016323d02e303fb5e0e3f57e46e9e4
Deryk Morrish <dmorrish@gmail.com>
2023-09-01 19:13:21 -0400

    Updated exception handling. Moved refresh_cache() into here instead of a dedicated command Cog.

--- respobot/commands/admin_commands.py
+++ respobot/commands/admin_commands.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord.commands import Option
 import constants
 import environment_variables as env
+import cache_races
 from slash_command_helpers import SlashCommandHelpers
 from discord.commands import SlashCommandGroup
 from irslashdata.exceptions import AuthenticationError, ServerDownError
@@ -39,57 +40,87 @@ class AdminCommandsCog(commands.Cog):
             choices=constants.PRONOUN_TYPES
         )
     ):
-        if not self.is_admin(ctx.user.id):
-            await ctx.respond(
-                "https://tenor.com/view/you-didnt-say-the-magic-word-ah-ah-nope-wagging-finger-gif-17646607"
+        try:
+            if not self.is_admin(ctx.user.id):
+                await ctx.respond(
+                    "https://tenor.com/view/you-didnt-say-the-magic-word-ah-ah-nope-wagging-finger-gif-17646607"
+                )
+                return
+
+            await ctx.respond("Working on it...")
+
+            discord_id = int(discord_id)
+
+            ir_member_dicts = await self.ir.get_member_info_new([iracing_custid])
+
+            if ir_member_dicts is None or len(ir_member_dicts) < 1:
+                await ctx.edit(content="Not a valid iRacing cust_id. Member not added.")
+                return
+
+            if len(ir_member_dicts) > 1:
+                await ctx.edit(
+                    content="More than one member dict was returned for some unknown reason. Member not added."
+                )
+                return
+
+            ir_member_since = ""
+            if 'member_since' in ir_member_dicts[0]:
+                ir_member_since = ir_member_dicts[0]['member_since']
+
+            member = None
+            try:
+                member = await ctx.guild.fetch_member(discord_id)
+            except (discord.HTTPException, discord.Forbidden) as exc:
+                await ctx.edit(
+                    content=f"The following exception occured when trying to fetch the discord Member object: {exc}"
+                )
+
+            if member is None:
+                await ctx.edit(content=f"This discord_id was not found in this server. Member not added.")
+
+            # We have finally passed all the checks. Add the member to the db.
+            try:
+                await self.db.add_member(name, iracing_custid, discord_id, ir_member_since, pronoun_type)
+            except BotDatabaseError as exc:
+                await ctx.edit(
+                    content=f"The following error occured when trying to add the member to the database: {exc}"
+                )
+                return
+            else:
+                await SlashCommandHelpers.refresh_members()
+                await ctx.edit(content="Success.")
+        except BotDatabaseError as exc:
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Error interfacing with the database.",
+                exc
             )
             return
-
-        await ctx.respond("Working on it...")
-
-        discord_id = int(discord_id)
-
-        try:
-            ir_member_dicts = await self.ir.get_member_info_new([iracing_custid])
-        except AuthenticationError:
-            await ctx.edit(content="Auth error. Fix yo shit!")
+        except AuthenticationError as exc:
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Authentication failed when trying to log in to the iRacing server.",
+                exc
+            )
             return
         except ServerDownError:
-            await ctx.edit(content="iRacing is down for maintenance. Try again later.")
-            return
-
-        if ir_member_dicts is None or len(ir_member_dicts) < 1:
-            await ctx.edit(content="Not a valid iRacing cust_id. Member not added.")
-            return
-
-        if len(ir_member_dicts) > 1:
-            await ctx.edit(content="More than one member dict was returned for some unknown reason. Member not added.")
-            return
-
-        ir_member_since = ""
-        if 'member_since' in ir_member_dicts[0]:
-            ir_member_since = ir_member_dicts[0]['member_since']
-
-        member = None
-        try:
-            member = await ctx.guild.fetch_member(discord_id)
-        except (discord.HTTPException, discord.Forbidden) as exc:
             await ctx.edit(
-                content=f"The following exception occured when trying to fetch the discord Member object: {exc}"
+                content=(
+                    f"iRacing is down for maintenance and this command relies on the stats server. "
+                    f"Try again later."
+                )
             )
-
-        if member is None:
-            await ctx.edit(content=f"This discord_id was not found in this server. Member not added.")
-
-        # We have finally passed all the checks. Add the member to the db.
-        try:
-            await self.db.add_member(name, iracing_custid, discord_id, ir_member_since, pronoun_type)
-        except BotDatabaseError as exc:
-            await ctx.edit(content=f"The following error occured when trying to add the member to the database: {exc}")
             return
-        else:
-            await SlashCommandHelpers.refresh_members()
-            await ctx.edit(content="Success.")
+        except (discord.HTTPException, discord.Forbidden, discord.InvalidArgument) as exc:
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Discord error.",
+                exc
+            )
+            return
 
     @admin_command_group.command(
         name='edit_member',
@@ -110,33 +141,27 @@ class AdminCommandsCog(commands.Cog):
             choices=constants.PRONOUN_TYPES
         )
     ):
-        if not self.is_admin(ctx.user.id):
-            await ctx.respond(
-                "https://tenor.com/view/you-didnt-say-the-magic-word-ah-ah-nope-wagging-finger-gif-17646607"
-            )
-            return
-
-        await ctx.respond("Working on it...")
-
-        member_split = member.split()
-        if member_split[0].isnumeric():
-            uid = int(member_split[0])
-        else:
-            await ctx.edit(content="Failed to parse the member uid from the member string.")
-            return
-
-        if remove is True:
-            try:
-                await self.db.remove_member(uid)
-                await ctx.edit(content=f"{member} successfully removed from the database.")
-            except BotDatabaseError as exc:
-                await ctx.edit(
-                    content=f"The following error occured when trying to remove {member} from the database: {exc}"
+        try:
+            if not self.is_admin(ctx.user.id):
+                await ctx.respond(
+                    "https://tenor.com/view/you-didnt-say-the-magic-word-ah-ah-nope-wagging-finger-gif-17646607"
                 )
                 return
-        else:
 
-            try:
+            await ctx.respond("Working on it...")
+
+            member_split = member.split()
+            if member_split[0].isnumeric():
+                uid = int(member_split[0])
+            else:
+                await ctx.edit(content="Failed to parse the member uid from the member string.")
+                return
+
+            if remove is True:
+                await self.db.remove_member(uid)
+                await ctx.edit(content=f"{member} successfully removed from the database.")
+
+            else:
                 # Get existing member info
                 existing_member_dict = await self.db.fetch_member_dict(uid=uid)
 
@@ -144,14 +169,7 @@ class AdminCommandsCog(commands.Cog):
                 ir_member_since = None
 
                 if refresh_ir:
-                    try:
-                        ir_member_dicts = await self.ir.get_member_info_new([iracing_custid])
-                    except AuthenticationError:
-                        await ctx.edit(content="Auth error. Fix yo shit!")
-                        return
-                    except ServerDownError:
-                        await ctx.edit(content="iRacing is down for maintenance. Try again later.")
-                        return
+                    ir_member_dicts = await self.ir.get_member_info_new([iracing_custid])
 
                     if ir_member_dicts is None or len(ir_member_dicts) < 1:
                         await ctx.edit(content="Not a valid iRacing cust_id. Member not edited.")
@@ -170,42 +188,55 @@ class AdminCommandsCog(commands.Cog):
                     if 'member_since' in ir_member_dicts[0]:
                         ir_member_since = ir_member_dicts[0]['member_since']
 
-                try:
-                    await self.db.edit_member(
-                        uid,
-                        name=name,
-                        iracing_custid=iracing_custid,
-                        discord_id=discord_id,
-                        ir_member_since=ir_member_since,
-                        pronoun_type=pronoun_type
-                    )
-                except BotDatabaseError as exc:
-                    await ctx.edit(
-                        content=f"The following error occured when trying to edit {member} in the database: {exc}"
-                    )
-                    return
-                else:
-                    if refresh_ir:
-                        try:
-                            await self.db.set_member_latest_session_found(iracing_custid, None)
-                        except BotDatabaseError as exc:
-                            await ctx.edit(
-                                content=(
-                                    f"The following error occured when trying to clear latest_session_found "
-                                    f"for {member} in the database: {exc}"
-                                )
-                            )
-                            return
+                await self.db.edit_member(
+                    uid,
+                    name=name,
+                    iracing_custid=iracing_custid,
+                    discord_id=discord_id,
+                    ir_member_since=ir_member_since,
+                    pronoun_type=pronoun_type
+                )
 
-                    if name is not None:
-                        await SlashCommandHelpers.refresh_members()
+                if refresh_ir:
+                    await self.db.set_member_latest_session_found(iracing_custid, None)
+
+                if name is not None:
+                    await SlashCommandHelpers.refresh_members()
 
                 await ctx.edit(content=f"{member} successfully edited in the database.")
-            except BotDatabaseError as exc:
-                await ctx.edit(
-                    content=f"The following error occured when trying to edit {member} in the database: {exc}"
+
+        except BotDatabaseError as exc:
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Error interfacing with the database.",
+                exc
+            )
+            return
+        except AuthenticationError as exc:
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Authentication failed when trying to log in to the iRacing server.",
+                exc
+            )
+            return
+        except ServerDownError:
+            await ctx.edit(
+                content=(
+                    f"iRacing is down for maintenance and this command relies on the stats server. "
+                    f"Try again later."
                 )
-                return
+            )
+            return
+        except (discord.HTTPException, discord.Forbidden, discord.InvalidArgument) as exc:
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Discord error.",
+                exc
+            )
+            return
 
     @admin_command_group.command(
         name='add_special_event',
@@ -226,20 +257,31 @@ class AdminCommandsCog(commands.Cog):
             choices=constants.IRACING_CATEGORIES
         )
     ):
-        if not self.is_admin(ctx.user.id):
-            await ctx.respond(
-                "https://tenor.com/view/you-didnt-say-the-magic-word-ah-ah-nope-wagging-finger-gif-17646607"
-            )
-            return
-
-        await ctx.respond("Working on it...")
-
         try:
+            if not self.is_admin(ctx.user.id):
+                await ctx.respond(
+                    "https://tenor.com/view/you-didnt-say-the-magic-word-ah-ah-nope-wagging-finger-gif-17646607"
+                )
+                return
+
+            await ctx.respond("Working on it...")
+
             await self.db.add_special_event(name, start_date, end_date, track, cars, category)
             await ctx.edit(content=f"Success. {name} added to the database.")
         except BotDatabaseError as exc:
-            await ctx.edit(
-                content=f"The following error occured when trying to add the special event to the database: {exc}"
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Error interfacing with the database.",
+                exc
+            )
+            return
+        except (discord.HTTPException, discord.Forbidden, discord.InvalidArgument) as exc:
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Discord error.",
+                exc
             )
             return
 
@@ -269,35 +311,26 @@ class AdminCommandsCog(commands.Cog):
             choices=constants.IRACING_CATEGORIES
         )
     ):
-        if not self.is_admin(ctx.user.id):
-            await ctx.respond(
-                "https://tenor.com/view/you-didnt-say-the-magic-word-ah-ah-nope-wagging-finger-gif-17646607"
-            )
-            return
-
-        await ctx.respond("Working on it...")
-
-        event_split = event.split()
-        if event_split[0].isnumeric():
-            uid = int(event_split[0])
-        else:
-            await ctx.edit(content="Failed to parse the event uid from the event string.")
-            return
-
-        if remove is True:
-            try:
-                await self.db.remove_special_event(uid)
-                await ctx.edit(content=f"{event} successfully removed from the database.")
-            except BotDatabaseError as exc:
-                await ctx.edit(
-                    content=(
-                        f"The following error occured when trying to remove the special event "
-                        f"{event} from the database: {exc}"
-                    )
+        try:
+            if not self.is_admin(ctx.user.id):
+                await ctx.respond(
+                    "https://tenor.com/view/you-didnt-say-the-magic-word-ah-ah-nope-wagging-finger-gif-17646607"
                 )
                 return
-        else:
-            try:
+
+            await ctx.respond("Working on it...")
+
+            event_split = event.split()
+            if event_split[0].isnumeric():
+                uid = int(event_split[0])
+            else:
+                await ctx.edit(content="Failed to parse the event uid from the event string.")
+                return
+
+            if remove is True:
+                await self.db.remove_special_event(uid)
+                await ctx.edit(content=f"{event} successfully removed from the database.")
+            else:
                 await self.db.edit_special_event(
                     uid,
                     name=name,
@@ -307,14 +340,66 @@ class AdminCommandsCog(commands.Cog):
                     category=category
                 )
                 await ctx.edit(content=f"{event} successfully edited in the database.")
-            except BotDatabaseError as exc:
-                await ctx.edit(
-                    content=(
-                        f"The following error occured when trying to edit the special event "
-                        f"{event} in the database: {exc}"
-                    )
+        except BotDatabaseError as exc:
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Error interfacing with the database.",
+                exc
+            )
+            return
+        except (discord.HTTPException, discord.Forbidden, discord.InvalidArgument) as exc:
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Discord error.",
+                exc
+            )
+            return
+
+    @admin_command_group.command(
+        name='refresh_cache',
+        description="Used by Deryk to rebuild the race cache from scratch."
+    )
+    async def admin_refresh_cache(
+        self,
+        ctx,
+        iracing_custid: Option(int, "iRacing id.", required=False)
+    ):
+        try:
+            if not self.is_admin(ctx.user.id):
+                await ctx.respond(
+                    "https://tenor.com/view/you-didnt-say-the-magic-word-ah-ah-nope-wagging-finger-gif-17646607"
                 )
                 return
+
+            await ctx.respond("Working on it...", ephemeral=True)
+            if iracing_custid is None:
+                await cache_races.cache_races(self.db, self.ir, await self.db.fetch_iracing_cust_ids())
+            else:
+                await cache_races.cache_races(self.db, self.ir, [iracing_custid])
+            try:
+                await ctx.edit(content="Done!")
+            except discord.HTTPException:
+                # Sometimes caching takes long enough to run that the webhook expires
+                # and editing the message fails.
+                return
+        except BotDatabaseError as exc:
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Error interfacing with the database.",
+                exc
+            )
+            return
+        except (discord.HTTPException, discord.Forbidden, discord.InvalidArgument) as exc:
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Discord error.",
+                exc
+            )
+            return
 
     def is_admin(self, discord_id):
         return discord_id == env.ADMIN_ID
