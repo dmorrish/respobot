e63ce347ed9870a779b488770704b6351844e2a7
Deryk Morrish <dmorrish@gmail.com>
2023-09-01 19:15:39 -0400

    Updated exception handling.

--- respobot/commands/cpi.py
+++ respobot/commands/cpi.py
@@ -9,6 +9,7 @@ import environment_variables as env
 import image_generators as image_gen
 import constants
 import stats_helpers as stats
+from bot_database import BotDatabaseError
 
 # other imports
 import os
@@ -36,73 +37,92 @@ class CornersPerIncidentCog(commands.Cog):
         member: Option(str, "Select a season", required=True, autocomplete=SlashCommandHelpers.get_member_list),
         series: Option(str, "Select a series", required=False, autocomplete=SlashCommandHelpers.get_all_series_list)
     ):
-        await ctx.respond("Working on it...")
+        try:
+            await ctx.respond("Working on it...")
 
-        series_id = None
+            series_id = None
 
-        series_name = ""
+            series_name = ""
 
-        (
-            current_year,
-            current_quarter,
-            _,
-            _,
-            _
-        ) = await self.db.get_current_iracing_week(series_id=constants.REFERENCE_SERIES)
+            (
+                current_year,
+                current_quarter,
+                _,
+                _,
+                _
+            ) = await self.db.get_current_iracing_week(series_id=constants.REFERENCE_SERIES)
 
-        if series is not None:
-            id_start = series.rfind("(") + 1
-            id_end = series.rfind(")")
-            series_id = int(series[id_start:id_end])
+            if series is not None:
+                id_start = series.rfind("(") + 1
+                id_end = series.rfind(")")
+                series_id = int(series[id_start:id_end])
 
-            if series_id is None:
-                await ctx.edit(content="Series not found. Make sure you select a series from the autocomplete list.")
+                if series_id is None:
+                    await ctx.edit(
+                        content="Series not found. Make sure you select a series from the autocomplete list."
+                    )
+                    return
+
+                series_name = await self.db.get_series_name_from_id(series_id)
+
+            member_dict = await self.db.fetch_member_dict(name=member)
+
+            cpi_graph_data = await stats.generate_cpi_graph_data(
+                self.db,
+                member_dict['iracing_custid'],
+                series_id=series_id
+            )
+
+            if len(cpi_graph_data) < 1:
+                await ctx.edit(content="This member has not raced this series.")
                 return
 
-            series_name = await self.db.get_series_name_from_id(series_id)
-
-        member_dict = await self.db.fetch_member_dict(name=member)
-
-        cpi_graph_data = await stats.generate_cpi_graph_data(
-            self.db,
-            member_dict['iracing_custid'],
-            series_id=series_id
-        )
-
-        if len(cpi_graph_data) < 1:
-            await ctx.edit(content="This member has not raced this series.")
-            return
-
-        total_corners = cpi_graph_data[-1][1]
-        if total_corners < constants.CPI_CORNERS_INCLUDED:
-            await ctx.edit(
-                content=(
-                    f"This member has not completed {constants.CPI_CORNERS_INCLUDED} corners "
-                    f"that match the selected options."
+            total_corners = cpi_graph_data[-1][1]
+            if total_corners < constants.CPI_CORNERS_INCLUDED:
+                await ctx.edit(
+                    content=(
+                        f"This member has not completed {constants.CPI_CORNERS_INCLUDED} corners "
+                        f"that match the selected options."
+                    )
                 )
+                return
+
+            member_dict["cpi_data"] = cpi_graph_data
+
+            title_text = f"Corners per Incident Graph for {member_dict['name']} ({cpi_graph_data[-1][2]:.1f})"
+            if series is not None:
+                title_text += f"\n{series_name}"
+            else:
+                title_text += f"\nAll Series"
+            graph = image_gen.generate_cpi_graph(member_dict, title_text, False)
+
+            filename = f"tmp_cpi_{str(datetime.now().strftime('%Y%m%d%H%M%S%f'))}.png"
+            filepath = env.BOT_DIRECTORY + env.MEDIA_SUBDIRECTORY + filename
+
+            graph.save(filepath, format=None)
+
+            with open(filepath, "rb") as f_graph:
+                picture = discord.File(f_graph)
+                await ctx.edit(content='', file=picture)
+                picture.close()
+
+            if os.path.exists(filepath):
+                os.remove(filepath)
+
+            return
+        except BotDatabaseError as exc:
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Error interfacing with the database.",
+                exc
             )
             return
-
-        member_dict["cpi_data"] = cpi_graph_data
-
-        title_text = f"Corners per Incident Graph for {member_dict['name']} ({cpi_graph_data[-1][2]:.1f})"
-        if series is not None:
-            title_text += f"\n{series_name}"
-        else:
-            title_text += f"\nAll Series"
-        graph = image_gen.generate_cpi_graph([member_dict], title_text, False)
-
-        filename = f"tmp_cpi_{str(datetime.now().strftime('%Y%m%d%H%M%S%f'))}.png"
-        filepath = env.BOT_DIRECTORY + env.MEDIA_SUBDIRECTORY + filename
-
-        graph.save(filepath, format=None)
-
-        with open(filepath, "rb") as f_graph:
-            picture = discord.File(f_graph)
-            await ctx.edit(content='', file=picture)
-            picture.close()
-
-        if os.path.exists(filepath):
-            os.remove(filepath)
-
-        return
+        except (discord.HTTPException, discord.Forbidden, discord.InvalidArgument) as exc:
+            await SlashCommandHelpers.process_command_failure(
+                self.bot,
+                ctx,
+                "Discord error.",
+                exc
+            )
+            return
