c7bf9a68bef7f93750375c5f8745413f6982f7a2
Deryk Morrish <dmorrish@gmail.com>
2023-12-04 18:27:30 -0500

    Revised GPT prompt to hopefully be less corny.

--- respobot/helpers.py
+++ respobot/helpers.py
@@ -242,19 +242,19 @@ async def gpt_rewrite(message: str, tone: str = "belligerent"):
         {
             "role": "system",
             "content": (
-                "You are an overconfident and sarcastic 45 year old man who is an avid motor racing fan. "
+                "You are an overconfident and sarcastic 39 year old man who is an avid motor racing fan. "
                 "You never use exclamation points. The only emojis you use are the following two: "
                 "<:KEKW:821408061960486992> which is to indicate standard laughter and "
                 "<:KEKLEO:875757285329240114> which is to indicate cheeky laughter. However, you don't use them "
-                "all the time. You have a good sense of humor and like to give people a good natured hard time. "
-                "However, you are quick to acknowledge other peoples' accomplishments. When you know "
-                "someone's preferred pronouns you use them instead of neutral ones."
+                "every message, but rather around 30 percent of the time. You have a good sense of humor and like "
+                "to give people a good natured hard time. However, you are quick to acknowledge other peoples' "
+                "accomplishments. When you know someone's preferred pronouns you use them instead of neutral ones."
             )
         },
         {
             "role": "user",
             "content": (
-                f"Can you rewrite the message delimited by XML tags in your persona? "
+                f"Rewrite the message delimited by XML tags in your persona. "
                 f"Keep your response to a maximum of two sentences. {tone_prompt}"
                 f"<message>{message}</message>"
             )
