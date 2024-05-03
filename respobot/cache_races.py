28c69c19cb2dbda31571867bf3f3486bbc00dbd0
Deryk Morrish <dmorrish@gmail.com>
2024-05-03 07:41:57 -0400

    Updated caching logic to more efficiently handle existing subsessions.

--- respobot/cache_races.py
+++ respobot/cache_races.py
@@ -69,15 +69,31 @@ async def cache_races(bot: discord.bot, db: BotDatabase, ir: IracingClient, irac
         # Get all hosted sessions
         caching_done = False
         start_time = datetime(date_started.year, 1, 1, tzinfo=timezone.utc)
-        end_time = start_time + timedelta(days=90)
         latest_session_end_time = None
 
         while caching_done is False:
+            year_rollover = False
+            end_time = start_time + timedelta(days=90)
+            if(end_time.year > start_time.year):
+                end_time = end_time.replace(year=start_time.year, month=12, day=31, hour=23, minute=59, second=59)
+                year_rollover = True
+
+            if end_time > datetime.now(timezone.utc):
+                end_time = datetime.now(timezone.utc)
+            end_time = end_time.replace(microsecond=0)
+
+            if end_time - start_time < timedelta(seconds=constants.FAST_LOOP_INTERVAL):
+                caching_done = True
+
             logging.getLogger('respobot.bot').info(
                 f"Gathering list of hosted subsessions for {start_time} to {end_time}."
             )
             finish_range_begin = start_time.isoformat().replace("+00:00", "Z")
             finish_range_end = end_time.isoformat().replace("+00:00", "Z")
+
+            if year_rollover is True:
+                end_time += timedelta(seconds=1)
+            start_time = end_time
 
             try:
                 hosted_results_dicts = await ir.search_hosted(
@@ -109,15 +125,6 @@ async def cache_races(bot: discord.bot, db: BotDatabase, ir: IracingClient, irac
             logging.getLogger('respobot.bot').info(str(len(hosted_results_dicts)) + " subsessions found.")
             # subsession_summary_dicts.append(hosted_results_dicts)
             subsession_summary_dicts += hosted_results_dicts
-
-            start_time = end_time
-            end_time = start_time + timedelta(days=90)
-            if end_time > datetime.now(timezone.utc):
-                end_time = datetime.now(timezone.utc)
-            end_time = end_time.replace(second=0, microsecond=0)
-
-            if end_time - start_time < timedelta(seconds=constants.FAST_LOOP_INTERVAL):
-                caching_done = True
 
         # Get all series sessions
         caching_done = False
@@ -158,14 +165,22 @@ async def cache_races(bot: discord.bot, db: BotDatabase, ir: IracingClient, irac
                 if latest_session_end_time is None or new_race_end_time > latest_session_end_time:
                     latest_session_end_time = new_race_end_time
 
-            new_subsession = await ir.subsession_data(subsession_summary_dict['subsession_id'])
-
-            if new_subsession is None:
-                continue
+            subsessionInDb = await db.is_subsession_in_db(subsession_summary_dict['subsession_id'])
+            subsessionLapsInDb = await db.is_subsession_in_laps_table(subsession_summary_dict['subsession_id'])
 
             try:
-                if await db.is_subsession_in_db(subsession_summary_dict['subsession_id']):
-                    continue
+                if subsessionInDb:
+                    if subsessionLapsInDb:
+                        logging.getLogger('respobot.bot').debug(
+                            f"Subsession {subsession_summary_dict['subsession_id']} already logged. Skipping."
+                        )
+                        continue
+                    else:
+                        logging.getLogger('respobot.bot').warning(
+                            f"Subsession {subsession_summary_dict['subsession_id']} in subsessions table"
+                            f" but not not laps table. Adding subsession."
+                        )
+
             except BotDatabaseError as exc:
                 logging.getLogger('respobot.bot').warning(
                     f"During cache_races() an exception was caught when checking if "
@@ -177,55 +192,83 @@ async def cache_races(bot: discord.bot, db: BotDatabase, ir: IracingClient, irac
                     f"During cache_races() an exception was caught when checking if "
                     f"subsession {subsession_summary_dict['subsession_id']} is in the database. "
                     f"Subsession skipped: Exception: {exc}"
+                )
+                continue
+
+            new_subsession = await ir.subsession_data(subsession_summary_dict['subsession_id'])
+
+            if new_subsession is None:
+                logging.getLogger('respobot.bot').warning(
+                    f"No subsession data returned for subsession {subsession_summary_dict['subsession_id']}. Skipping."
                 )
                 continue
 
             # Add the new race to the database
-            try:
-                await db.add_subsessions([new_subsession])
-            except BotDatabaseError as exc:
-                logging.getLogger('respobot.bot').warning(
-                    f"During cache_races() an exception was encountered when adding "
-                    f"subsession {new_subsession['subsession_id']} to the database: {exc}"
+            if subsessionInDb is False:
+                logging.getLogger('respobot.bot').debug(
+                    f"Adding subsession {subsession_summary_dict['subsession_id']} to subsessions and results table."
                 )
-                await helpers.send_bot_failure_dm(
-                    bot,
-                    f"During cache_races() an exception was encountered when adding "
-                    f"subsession {new_subsession['subsession_id']} to the database: {exc}"
-                )
-
-            # And now for the laps
-            if 'session_results' not in new_subsession or len(new_subsession['session_results']) < 1:
-                continue
-
-            for session_result_dict in new_subsession['session_results']:
-                if 'simsession_number' not in session_result_dict or 'results' not in session_result_dict:
-                    continue
-
-                lap_dicts = await ir.lap_data(
-                    new_subsession['subsession_id'],
-                    session_result_dict['simsession_number']
-                )
-
-                if lap_dicts is None or len(lap_dicts) < 1:
-                    continue
-
                 try:
-                    await db.add_laps(
-                        lap_dicts,
-                        new_subsession['subsession_id'],
-                        session_result_dict['simsession_number']
-                    )
+                    await db.add_subsessions([new_subsession])
                 except BotDatabaseError as exc:
                     logging.getLogger('respobot.bot').warning(
                         f"During cache_races() an exception was encountered when adding "
-                        f"laps to the database: {exc}"
+                        f"subsession {new_subsession['subsession_id']} to the database: {exc}"
                     )
                     await helpers.send_bot_failure_dm(
                         bot,
                         f"During cache_races() an exception was encountered when adding "
-                        f"laps to the database: {exc}"
+                        f"subsession {new_subsession['subsession_id']} to the database: {exc}"
                     )
+
+            # And now for the laps
+            if subsessionLapsInDb is False:
+                logging.getLogger('respobot.bot').debug(
+                    f"Adding subsession {subsession_summary_dict['subsession_id']} laps to laps table."
+                )
+                if 'session_results' not in new_subsession or len(new_subsession['session_results']) < 1:
+                    logging.getLogger('respobot.bot').warning(
+                        f"No subsession results for subsession {subsession_summary_dict['subsession_id']}."
+                        f" Skipping laps."
+                    )
+                    continue
+
+                for session_result_dict in new_subsession['session_results']:
+                    if 'simsession_number' not in session_result_dict or 'results' not in session_result_dict:
+                        logging.getLogger('respobot.bot').warning(
+                            f"No simsession_number in session_result_dict for subsession"
+                            f" {subsession_summary_dict['subsession_id']}. Skipping laps."
+                        )
+                        continue
+
+                    lap_dicts = await ir.lap_data(
+                        new_subsession['subsession_id'],
+                        session_result_dict['simsession_number']
+                    )
+
+                    if lap_dicts is None or len(lap_dicts) < 1:
+                        logging.getLogger('respobot.bot').warning(
+                            f"No laps returned by iR stats server for subsession"
+                            f" {subsession_summary_dict['subsession_id']}. Skipping laps."
+                        )
+                        continue
+
+                    try:
+                        await db.add_laps(
+                            lap_dicts,
+                            new_subsession['subsession_id'],
+                            session_result_dict['simsession_number']
+                        )
+                    except BotDatabaseError as exc:
+                        logging.getLogger('respobot.bot').warning(
+                            f"During cache_races() an exception was encountered when adding "
+                            f"laps to the database: {exc}"
+                        )
+                        await helpers.send_bot_failure_dm(
+                            bot,
+                            f"During cache_races() an exception was encountered when adding "
+                            f"laps to the database: {exc}"
+                        )
 
         # If the person has never done a hosted session or a race, set the latest_session_found to two days ago
         # to make sure we don't miss a session in the off chance they were added right while they were finishing
